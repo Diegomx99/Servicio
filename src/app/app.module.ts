cf5c9940b9187c210f70b4c3f99798dd503ed093
marcosonofremoreno <77844873+marcosonofremoreno@users.noreply.github.com>
2021-03-27 13:27:39 -0700

    Creacion de componentes y su rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+//Rutas
+import { App_routing } from './app.routes';
+
+//Componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { QrComponent } from './components/qr/qr.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { LoginComponent } from './components/login/login.component';
+import { PropietariosComponent } from './components/propietarios/propietarios.component';
+import { ActivosComponent } from './components/activos/activos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    QrComponent,
+    UsuariosComponent,
+    LoginComponent,
+    PropietariosComponent,
+    ActivosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    App_routing
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,20 @@
+import { RouterModule, Routes} from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { QrComponent } from './components/qr/qr.component';
+import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { LoginComponent } from './components/login/login.component';
+import { PropietariosComponent } from './components/propietarios/propietarios.component';
+import { ActivosComponent } from './components/activos/activos.component';
+
+
+const app_routes : Routes =[
+    { path:'home',component: HomeComponent },
+    { path:'qr',component: QrComponent },
+    { path:'usuarios',component: UsuariosComponent},
+    { path:'login',component: LoginComponent},
+    { path:'propietarios',component: PropietariosComponent},
+    { path:'activos',component: ActivosComponent},
+    { path:'**', pathMatch: 'full', redirectTo:'home'}
+]
+
+export const App_routing = RouterModule.forRoot(app_routes);
