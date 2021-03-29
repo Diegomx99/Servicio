2c2779105af41ba116432ed874f2c2554edd00a0
marcosonofremoreno <77844873+marcosonofremoreno@users.noreply.github.com>
2021-03-29 05:10:29 -0700

    Agregado edificio y salon

--- src/app/components/qr/qr.component.ts
+++ src/app/components/qr/qr.component.ts
@@ -10,35 +10,42 @@ export class QrComponent implements OnInit {
 
   public qrCodeVal: string = null;
   public Nombre: string = null;
-  public Ubicacion: string = null;
+  public Edificio: string = null;
+  public Salon: string = null;
   public Cantidad: string = null;
   public Propietario: string = null;
   public img: CanvasImageData ;
 
   getNombre(nombre){
       this.Nombre = nombre;
-      this.qrCodeVal = "Activo: " + this.Nombre + " " + "Apellido: "+  this.Ubicacion;
+      this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Edificio: "+  this.Edificio + " " + "Salon: " + this.Salon + " " + "Cantidad: " + this.Cantidad + " " + "Propietario: " + this.Propietario;
   }
 
-  getUbicacion(ubicacion){
-    this.Ubicacion = ubicacion;
-    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Ubicacion: "+  this.Ubicacion;
+  getEdificio(edificio){
+    this.Edificio = edificio;
+    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Edificio: "+  this.Edificio + " " + "Salon: " + this.Salon + " " + "Cantidad: " + this.Cantidad + " " + "Propietario: " + this.Propietario;
+  }
+
+  getSalon(salon){
+    this.Salon = salon;
+    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Edificio: "+  this.Edificio + " " + "Salon: " + this.Salon + " " + "Cantidad: " + this.Cantidad + " " + "Propietario: " + this.Propietario;
   }
   
   getCantidad(cantidad){
     this.Cantidad = cantidad;
-    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Ubicacion: "+  this.Ubicacion + " " + "Cantidad: " + this.Cantidad;
+    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Edificio: "+  this.Edificio + " " + "Salon: " + this.Salon + " " + "Cantidad: " + this.Cantidad + " " + "Propietario: " + this.Propietario;
   }
 
   getPropietario(propietario){
     this.Propietario = propietario;
-    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Ubicacion: "+  this.Ubicacion + " " + "Cantidad: " + this.Cantidad + " " + "Propietario: " + this.Propietario;
+    this.qrCodeVal = "Nombre: " + this.Nombre + " " + "Edificio: "+  this.Edificio + " " + "Salon: " + this.Salon + " " + "Cantidad: " + this.Cantidad + " " + "Propietario: " + this.Propietario;
   }
   
   clickRestablecer(){
     this.qrCodeVal = null;
     this.Nombre = null;
-    this.Ubicacion = null;
+    this.Edificio = null;
+    this.Salon = null;
     this.Cantidad = null;
     this.Propietario = null;
   }
@@ -86,13 +93,16 @@ export class QrComponent implements OnInit {
   saveAsImage() {
     const fileNameToDownload = 'image_qrcode';
     const base64Img = document.getElementById('QR')['src'];
+    
     fetch(base64Img)
        .then(res => res.blob())
        .then((blob) => {
           // IE
+          blob = new Blob([base64Img], { type: "image/png" });
           if (window.navigator && window.navigator.msSaveOrOpenBlob){
              window.navigator.msSaveOrOpenBlob(blob,fileNameToDownload);
           } else { // Chrome
+            
              const url = window.URL.createObjectURL(blob);
              const link = document.createElement('a');
              link.href = url;
@@ -100,7 +110,25 @@ export class QrComponent implements OnInit {
              link.click();
           }
        })
+       
  }
+
+ private convertBase64ToBlob(Base64Image: any) {
+  // SPLIT INTO TWO PARTS
+  const parts = Base64Image.split(';base64,');
+  // HOLD THE CONTENT TYPE
+  const imageType = parts[0].split(':')[1];
+  // DECODE BASE64 STRING
+  const decodedData = window.atob(parts[1]);
+  // CREATE UNIT8ARRAY OF SIZE SAME AS ROW DATA LENGTH
+  const uInt8Array = new Uint8Array(decodedData.length);
+  // INSERT ALL CHARACTER CODE INTO UINT8ARRAY
+  for (let i = 0; i < decodedData.length; ++i) {
+    uInt8Array[i] = decodedData.charCodeAt(i);
+  }
+  // RETURN BLOB IMAGE AFTER CONVERSION
+  return new Blob([uInt8Array], { type: imageType });
+}
   constructor() { 
  
   }
